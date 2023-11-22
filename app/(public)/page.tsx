c9b9b6423a08153f50ad2acc0373f5afd917e2fb
Travis Luong <travis@travisluong.com>
2023-11-22 10:17:12 +0800

    build the home page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,3 +1,27 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <h1 className="bg-yellow-300 p-2 text-center font-bold text-black">
+        Markdown App
+      </h1>
+      <div>
+        <Link
+          href="/signup"
+          className="bg-red-700 p-2 text-center block text-white"
+        >
+          Sign Up
+        </Link>
+      </div>
+      <div>
+        <Link
+          href="/login"
+          className="bg-blue-700 p-2 text-center block text-white"
+        >
+          Log In
+        </Link>
+      </div>
+    </div>
+  );
 }
