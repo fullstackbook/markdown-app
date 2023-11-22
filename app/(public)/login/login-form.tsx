4ae4e6cbfd38918b2aaa3c4518c08a3f04f3d132
Travis Luong <travis@travisluong.com>
2023-11-22 14:56:55 +0800

    style the login form

--- app/(public)/login/login-form.tsx
+++ app/(public)/login/login-form.tsx
@@ -2,18 +2,32 @@
 
 export default function LoginForm() {
   return (
-    <div>
-      <h1>Log In</h1>
-      <form>
-        <div>
+    <div className="flex flex-col gap-2 w-full">
+      <h1 className="bg-yellow-300 p-2 font-bold text-center text-black">
+        Log In
+      </h1>
+      <form className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2">
           <label>Username</label>
-          <input id="username" name="username" type="text" />
+          <input
+            id="username"
+            name="username"
+            type="text"
+            className="bg-blue-700 p-2 text-white block"
+          />
         </div>
-        <div>
+        <div className="flex flex-col gap-2">
           <label>Password</label>
-          <input id="password" name="password" type="password" />
+          <input
+            id="password"
+            name="password"
+            type="password"
+            className="bg-blue-700 p-2 text-white block"
+          />
         </div>
-        <button type="submit">Submit</button>
+        <button type="submit" className="bg-red-700 text-white p-2 mt-3">
+          Submit
+        </button>
       </form>
     </div>
   );
