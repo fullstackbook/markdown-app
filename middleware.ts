32085b33125070e5df895fe52b2674a5afa540c5
Travis Luong <travis@travisluong.com>
2023-11-27 15:02:09 +0800

    authenticate notes api routes

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   const authenticatedRoutes = [pathname.startsWith("/dashboard")];
+  const authenticatedAPIRoutes = [pathname.startsWith("/api/notes")];
 
   if (authenticatedRoutes.includes(true)) {
     const cookie = request.cookies.get("jwt-token");
@@ -23,6 +24,22 @@ export async function middleware(request: NextRequest) {
       const url = request.nextUrl.clone();
       url.pathname = "/login";
       return NextResponse.redirect(url);
+    }
+  }
+
+  if (authenticatedAPIRoutes.includes(true)) {
+    const cookie = request.cookies.get("jwt-token");
+
+    if (!cookie || !cookie?.value) {
+      return NextResponse.json({ message: "unauthenticated" }, { status: 401 });
+    }
+
+    try {
+      const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+      await jwtVerify(cookie.value, secret);
+    } catch (error) {
+      console.error(error);
+      return NextResponse.json({ message: "unauthenticated" }, { status: 401 });
     }
   }
 }
