004af805bb1947e2813f1187d7cbce41625d7321
Travis Luong <travis@travisluong.com>
2023-11-27 14:13:57 +0800

    use the notes provider

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,17 @@
+"use client";
+
+import { NotesProvider } from "../contexts/notes-context";
+
 export default function Layout(props: {
   sidebar: React.ReactNode;
   content: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-row">
-      {props.sidebar}
-      {props.content}
-    </div>
+    <NotesProvider>
+      <div className="flex flex-row">
+        {props.sidebar}
+        {props.content}
+      </div>
+    </NotesProvider>
   );
 }
